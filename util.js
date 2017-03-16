a8288aa45f767a0208d032957ecba315b74653a6
klockett <kelockett@fullsail.edu>
2017-03-16 12:55:00 -0700

    update
    update

---
+++ util.js
@@ -0,0 +1,15 @@
+var chalk = require('chalk');
+
+exports.debug = (title, obj, status) => {
+  var seperator = '\n==================================\n';
+  const output = seperator + title + seperator;
+
+  var error = chalk.bold.red;
+  console.log(error('Error!'));
+  if (!status) {
+    status = '';
+  }
+  if (process.env.DEBUG) {
+    console.log(error(output), obj, status);
+  }
+};
