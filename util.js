5e5dd52e7f87714b81f91d920135c82ea0da7827
milemu23 <design@michellemunksgard.com>
2017-03-23 23:32:35 -0400

    Update util.js
    Added the version bumper. Will add additional files after this.

--- util.js
+++ util.js
@@ -1,15 +1,57 @@
-var chalk = require('chalk');
+const chalk = require('chalk');
+
+exports.bump = (currentVer, typeofIncrement) => {
+  // major [0]
+  // minor [1]
+  // patch [2]
+  const aryVers = currentVer.split('.');
+
+  for (var i in aryVers) {
+    aryVers[i] = parseInt(aryVers[i], 10);
+  }
+
+  if (typeofIncrement === 'patch') {
+    aryVers[2] += 1;
+  } else if (typeofIncrement === 'minor') {
+    aryVers[2] = 0;
+    aryVers[1] += 1;
+  } else if (typeofIncrement === 'major') {
+    aryVers[2] = 0;
+    aryVers[1] = 0;
+    aryVers[0] += 1;
+  }
+  return aryVers.join('.');
+};
+
 
 exports.debug = (title, obj, status) => {
-  var seperator = '\n==================================\n';
-  const output = seperator + title + seperator;
+  if (process.env.DEBUG === 'true') {
+    const seperator = '\n==================================\n';
+    const output = seperator + title + seperator;
 
-  var error = chalk.bold.red;
-  console.log(error('Error!'));
-  if (!status) {
-    status = '';
-  }
-  if (process.env.DEBUG) {
-    console.log(error(output), obj, status);
+    const error = chalk.bold.red;
+    const success = chalk.bold.green;
+    const warn = chalk.bold.yellow;
+
+    if (!status) {
+      status = 'There is no status';
+    }
+
+    switch (status) {
+      case 'error!':
+      case 'error':
+      case 'err':
+        console.error(error(output), obj, status);
+        break;
+      case 'success!':
+      case 'success':
+        console.log(success(output));
+        break;
+      case 'warning!':
+      case 'warn':
+      case 'warning':
+        console.warn(warn(output));
+        break;
+    }
   }
 };
